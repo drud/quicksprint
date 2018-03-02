6aae87dd7b6a4d233d24fdf499f7e37ff3bcdf1c
Randy Fay <randy@randyfay.com>
2018-03-01 18:57:45 -0700

    First pass at install_ddev.cmd

--- install_ddev.cmd
+++ install_ddev.cmd
@@ -7,26 +7,11 @@ fi
 
 echo "Installing docker images for ddev to use..."
 cd ddev_tarballs
-7z x ddev_docker_images*.gz
+../bin/7za x ddev_docker_images*.gz
 docker load -i ddev_docker_images*.tar
 
-unzip $TARBALL -C /tmp
-chmod ugo+x /tmp/ddev
+../bin/7za x ddev_windows*.zip
+copy ddev.exe %HOMEPATH%\AppData\Local\Microsoft\WindowsApps
 
-printf "Ready to place ddev in your /usr/local/bin.\n"
 
-if [[ "$BINOWNER" == "$USER" ]]; then
-    mv /tmp/ddev /usr/local/bin/
-else
-    printf "${YELLOW}Running \"sudo mv /tmp/ddev /usr/local/bin/\" Please enter your password if prompted.${RESET}\n"
-    sudo mv /tmp/ddev /usr/local/bin/
-fi
-
-if which brew &&  [ -f `brew --prefix`/etc/bash_completion ]; then
-	bash_completion_dir=$(brew --prefix)/etc/bash_completion.d
-    cp /tmp/ddev_bash_completion.sh $bash_completion_dir/ddev
-    printf "${GREEN}Installed ddev bash completions in $bash_completion_dir${RESET}\n"
-    rm /tmp/ddev_bash_completion.sh
-fi
-
-printf "${GREEN}ddev is now installed. Run \"ddev\" to verify your installation and see usage.${RESET}\n"
+echo "ddev is now installed. Run \"ddev\" to verify your installation and see usage."
