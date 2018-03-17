79008d6084d113368e16daaf8054b687b40b852f
Brian Gilbert <brian@realityloop.com>
2018-03-18 06:49:22 +1100

    Simplification of additions script suggested bt @mradcliffe

--- package_additions.sh
+++ package_additions.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # Create archive with docker additions for sprint (IDE and IRC).
-docker save -o ${REPO_DIR}/docker_additions.tar briangilbert/cloud9-alpine:latest linuxserver/thelounge:latest
-tar cfJ ${STAGING_DIR}/ddev_tarballs/docker_additions.tar.xz ${REPO_DIR}/docker_additions.tar
-rm ${REPO_DIR}/docker_additions.tar
+docker save briangilbert/cloud9-alpine:latest linuxserver/thelounge:latest | xz -z -9 > ${REPO_DIR}/docker_additions.tar.xz
