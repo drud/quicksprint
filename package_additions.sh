f6f4f4ae40eeea1274fa182b08e333476aed9425
Brian Gilbert <brian@realityloop.com>
2018-03-18 19:23:02 +1100

    Update tag for cloud9 image.

--- package_additions.sh
+++ package_additions.sh
@@ -4,5 +4,5 @@ set -o pipefail
 set -o nounset
 
 # Create archive with docker additions for sprint (IDE and IRC).
-docker pull briangilbert/cloud9-alpine:201803 linuxserver/thelounge:94
-docker save briangilbert/cloud9-alpine:201803 linuxserver/thelounge:94 | xz -z -9 > $STAGING_DIR/ddev_tarballs/docker_additions.tar.xz
+docker pull briangilbert/cloud9-alpine:20180318 linuxserver/thelounge:94
+docker save briangilbert/cloud9-alpine:20180318 linuxserver/thelounge:94 | xz -z -9 > $STAGING_DIR/ddev_tarballs/docker_additions.tar.xz
