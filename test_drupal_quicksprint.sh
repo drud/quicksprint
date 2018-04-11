a3c92407098d23d56633236bce6f8b276b36d4d2
Randy Fay <randy@randyfay.com>
2018-04-10 19:53:59 -0600

    Move tests into test_drupal_quicksprint

---
+++ test_drupal_quicksprint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Initial setup: Make sure we have no ddev
+if which ddev; then
+  echo "ddev is already on this system"
+  exit 1
+fi
+
+# Untar the sprint package (no docker version should be fine)
+tar -C /tmp -zxf ~/tmp/drupal_sprint_package.no_docker.$(cat ./.quicksprint_release.txt).tar.gz
+# Run the standard install_ddev.sh, asserting we already have docker set up.
+cd /tmp/drupal_sprint_package && echo y | ./install_ddev.sh
+
+# Remove any existing sprints, then do start_sprint.sh
+cd ~/Sites/sprint && chmod -R u+w sprint-* && rm -rf sprint-* echo y | ./start_sprint.sh
+
+# Run start_sprint in the (only) sprint directory
+cd ~/Sites/sprint/sprint-2* && echo y | ./start_clean.sh
+
+rm -r /tmp/drupal_sprint_package
+
+echo "Test successful"
