b871859aba89187926b8599c52a04aa6fa8eb90e
Matthew Radcliffe <mradcliffe@softpixel.com>
2018-08-04 15:04:11 -0400

    Adds some tests of ddev health and web container

--- test_drupal_quicksprint.sh
+++ test_drupal_quicksprint.sh
@@ -24,6 +24,14 @@ echo y | ./start_sprint.sh
 # Run start_sprint in the (only) sprint directory
 cd ~/Sites/sprint/sprint-2* && echo y | ./start_clean.sh
 
+# Confirms web and router status.
+ddev describe | grep -q running || exit 1
+ddev describe | grep -q "DDEV ROUTER STATUS: healthy" || exit 1
+
+# Confirms site is responding with 200.
+SITE_URL=`ddev describe | grep -o -m 1 "https://sprint-[0-9\-]\+\.ddev\.local"`
+curl -k -L -I --silent ${SITE_URL}:8443 | head -n 1 | grep -q 200 || echo "ERROR: Could not connect to ${SITE_URL}:8443!"; exit 1
+
 rm -r /tmp/drupal_sprint_package
 
 echo "Test successful"
