1b0da3019a202fb3ab6f146ff9d0ccbea1c7bd5c
Randy Fay <randy@randyfay.com>
2018-08-16 21:27:38 -0600

    Fix location of sprint.tar.xz

--- package_drupal_script.sh
+++ package_drupal_script.sh
@@ -163,7 +163,7 @@ cd $STAGING_DIR
 
 echo "Creating tar and zipballs"
 # Create tar.xz archive without using xz command, so we can work on all platforms
-pushd sprint && 7z a -ttar -so bogusfilename.tar . | 7z a -si -txz sprint.tar.xz && popd >/dev/null
+pushd sprint && 7z a -ttar -so bogusfilename.tar . | 7z a -si -txz ../sprint.tar.xz && popd >/dev/null
 rm -rf $STAGING_DIR/sprint
 
 cd $STAGING_DIR_BASE
