4f979d8cc1660589d9acedf721401e6a570240a9
Randy Fay <randy@randyfay.com>
2018-08-16 21:46:03 -0600

    Remove time from test.sh to see if it helps

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -6,6 +6,6 @@ set -o nounset
 
 echo "--- buildkite building $BUILDKITE_JOB_ID at $(date) on $(hostname) for OS=$(go env GOOS) in $PWD"
 echo "--- package_drupal_script.sh"
-echo "n" | time ./package_drupal_script.sh
+echo "n" | ./package_drupal_script.sh
 echo "--- test_drupal_quicksprint.sh"
 time ./test_drupal_quicksprint.sh
