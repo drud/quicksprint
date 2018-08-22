9dcde5ed1e69fee24e8ee84bd0e3aacd76292876
Randy Fay <randy@randyfay.com>
2018-08-22 13:20:07 -0600

    Minor docs improvements and script improvement (#84)

--- install.sh
+++ install.sh
@@ -82,29 +82,29 @@ if [ ! -z "$TARBALL" ] ; then
     tar -xzf ${TARBALL} -C /tmp
     chmod ugo+x /tmp/ddev
 
-    if command -v ddev >/dev/null && [  -z "${DDEV_INSTALL_DIR}" ] ; then
+    if command -v ddev >/dev/null && [  -z "${DDEV_INSTALL_DIR:-}" ] ; then
         printf "${RED}A version of ddev already exists in $(command -v ddev); You may upgrade it using your normal upgrade technique. Not installing a new version.${RESET}"
     else
         # Calling script may have already set DDEV_INSTALL_DIR, otherwise we respect and use it.
         # Otherwise, try to use /usr/local/bin and then /usr/bin
-        if [ ! -z "${DDEV_INSTALL_DIR}" ]; then
+        if [ ! -z "${DDEV_INSTALL_DIR:-}" ]; then
             # It's the responsibility of the caller to have created the directory
             # and to have added the directory to $PATH
-            echo "Installing for tests into ${DDEV_INSTALL_DIR}"
+            echo "Installing for tests into DDEV_INSTALL_DIR='${DDEV_INSTALL_DIR:-}'"
         fi
         DDEV_INSTALL_DIR=${DDEV_INSTALL_DIR:-/usr/local/bin}
-        if [ ! -d ${DDEV_INSTALL_DIR} ] ; then
-            echo "DDEV_INSTALL_DIR ${DDEV_INSTALL_DIR} does not exist"
+        if [ ! -d ${DDEV_INSTALL_DIR:-} ] ; then
+            echo "DDEV_INSTALL_DIR '${DDEV_INSTALL_DIR:-}' does not exist"
             exit 3
         fi
-        printf "Ready to place ddev in directory $DDEV_INSTALL_DIR.\n"
-        BINOWNER=$(ls -ld $DDEV_INSTALL_DIR | awk '{print $3}')
+        printf "Ready to place ddev in directory ${DDEV_INSTALL_DIR:-}.\n"
+        BINOWNER=$(ls -ld ${DDEV_INSTALL_DIR:-} | awk '{print $3}')
 
         if [[ "$BINOWNER" == "$USER" ]]; then
-            mv -f /tmp/ddev ${DDEV_INSTALL_DIR}
+            mv -f /tmp/ddev ${DDEV_INSTALL_DIR:-}
         else
             printf "${YELLOW}Running \"sudo mv /tmp/ddev $DDEV_INSTALL_DIR\" Please enter your password if prompted.${RESET}\n"
-            sudo mv /tmp/ddev ${DDEV_INSTALL_DIR}
+            sudo mv /tmp/ddev ${DDEV_INSTALL_DIR:-}
         fi
     fi
 fi
@@ -126,3 +126,7 @@ ${GREEN}
 ######
 ${RESET}
 "
+
+if ! command -v ddev >/dev/null && [ ${OS} = "MINGW64_NT-10.0" ] ; then
+    printf "${RED}ddev has not yet been installed. Please use the ddev_windows_installer to install it${RESET}"
+fi
