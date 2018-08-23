62c435c9f4378090b5468f22f0f5d8fafa0f8936
Randy Fay <randy@randyfay.com>
2018-08-23 12:07:39 -0600

    Clean up sprint packages on buildkite build (#89)

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -6,6 +6,7 @@ set -o nounset
 
 echo "--- buildkite building at $(date) on $(hostname) for OS=$(go env GOOS) in $PWD"
 echo "--- package_drupal_script.sh"
+rm -f ~/tmp/drupal_sprint_package*gz ~/tmp/drupal_sprint_package*zip
 echo "n" | ./package_drupal_script.sh
 echo "--- test_drupal_quicksprint.sh"
 tests/test_drupal_quicksprint.sh
