ce886d524ef58a174f69e41465673dd5e1010167
Randy Fay <randy@randyfay.com>
2019-01-17 17:04:36 -0700

    Minor improvements to sanetestbot to make sure if ddev is installed it's recent

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "--- buildkite building at $(date) on $(hostname) for OS=$(go env GOOS) in $PWD with golang=$(go version) docker=$(docker version --format '{{.Server.Version}}') and docker-compose $(docker-compose version --short) ddev version=$(ddev version -j | jq -r .raw.cli)"
+
 set -o errexit
 set -o pipefail
 set -o nounset
 
-echo "--- buildkite building at $(date) on $(hostname) for OS=$(go env GOOS) in $PWD"
 echo "--- package_drupal_script.sh"
 rm -f ~/tmp/drupal_sprint_package*gz ~/tmp/drupal_sprint_package*zip
 echo "n" | ./package_drupal_script.sh
