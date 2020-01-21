fda314d86e20464af7323b19bc83442504d34fbb
Randy Fay <randy@randyfay.com>
2020-01-20 17:05:23 -0700

    Fix problem with switch_branch and do the install

--- sprint/switch_branch.sh
+++ sprint/switch_branch.sh
@@ -3,7 +3,7 @@
 # This script allows switching drupal branch, for example, from 9.0.x to 8.9.x
 # or back
 
-set -o errexit pipefail nounset
+set -o errexit -o pipefail -o nounset
 
 if [[ $# != 1 ]]; then
   echo "Please provide a branch to switch to. For example './switch_branch.sh 8.9.x'"
@@ -20,7 +20,8 @@ ddev exec  "git fetch && git stash save && git checkout origin/${target_branch}"
 ddev composer install
 ddev exec "( git stash apply || true )"
 echo "DROP DATABASE db; CREATE DATABASE db; " | ddev mysql -uroot -proot
+ddev exec drush si --yes standard --account-pass=admin --db-url=mysql://db:db@db/db --site-name='Drupal Contribution Time!'
 set +x
-popd
-echo "Switched to ${target_branch}; you can now install via the web installer"
+popd >/dev/null
+echo "Switched to ${target_branch}"
 ddev list
