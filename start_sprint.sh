dfd3b9256a7067f933eaa82772a82aa4c0930488
Randy Fay <randy@randyfay.com>
2020-03-18 21:54:50 -0600

    Use mariadb 10.3 for latest drupal (#170)
    MariaDB is now required by Drupal 9.0.x, see https://www.drupal.org/project/drupal/issues/3120124

--- start_sprint.sh
+++ start_sprint.sh
@@ -34,7 +34,7 @@ fi
 cd "${SPRINTNAME}/drupal"
 echo "Using ddev version $(ddev version| awk '/^cli/ { print $2}') from $(which ddev)"
 
-ddev config --docroot . --project-type drupal8 --php-version=7.3 --http-port=8080 --https-port=8443 --project-name="sprint-${TIMESTAMP}"
+ddev config --docroot . --project-type drupal8 --php-version=7.3 --mariadb-version=10.3 --http-port=8080 --https-port=8443 --project-name="sprint-${TIMESTAMP}"
 
 ddev config global --instrumentation-opt-in=false >/dev/null
 printf "${YELLOW}Configuring your fresh Drupal8 instance. This takes a few minutes.${RESET}\n"
